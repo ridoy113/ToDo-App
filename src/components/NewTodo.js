16613ec3a012c5bd28b934c42ffdc99180f99e10
ridoy113 <almamounhridoy113@gmail.com>
2022-09-22 16:10:25 +0600

    take data form value

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './newtodo.module.css';
 
 
 
 
 const NewTodo = () => {
+
+    const [todo, setTodo] = useState({ title: "", desc: "" });
+
+    const { title, desc } = todo;
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        setTodo((oldTodo) => {
+            return { ...oldTodo, [name]: event.target.value }
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(todo);
+    }
+
     return (
-        <form className={style.form}>
+        <form className={style.form} onSubmit={handleSubmit}>
 
             <div className={style["form-field"]}>
                 <label htmlFor="title">Title: </label>
-                <input type="text" name="title" id="title" />
+                <input type="text" name="title" id="title" value={title} onChange={handleChange} />
             </div>
 
             <div className={style["form-field"]}>
                 <label htmlFor="desc">Description: </label>
-                <textarea type="text" name="desc" id="desc" />
+                <textarea type="text" name="desc" id="desc" value={desc} onChange={handleChange} />
             </div>
 
             <button type='submit'>Add Todo</button>
